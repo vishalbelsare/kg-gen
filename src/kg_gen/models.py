5b6fbaecd7791b5672f9b168ee002421d4b2eb1f
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2025-10-14 01:21:17 -0500

    Update graph model to read/write graph from files

--- src/kg_gen/models.py
+++ src/kg_gen/models.py
@@ -1,3 +1,4 @@
+import json
 from pydantic import BaseModel, Field
 from typing import Tuple, Optional
 
@@ -13,3 +14,37 @@ class Graph(BaseModel):
     )
     entity_clusters: Optional[dict[str, set[str]]] = None
     edge_clusters: Optional[dict[str, set[str]]] = None
+
+    @staticmethod
+    def from_file(file_path: str) -> "Graph":
+        """
+        Load the graph from a file.
+        Fix graph entities and edges for missing ones defined in relations.
+        """
+        with open(file_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            graph = Graph.model_validate(data)
+
+        # Fix graph entities and edges
+        for relation in graph.relations:
+            if relation[0] not in graph.entities:
+                graph.entities.add(relation[0])
+            if relation[1] not in graph.edges:
+                graph.edges.add(relation[1])
+            if relation[2] not in graph.entities:
+                graph.entities.add(relation[2])
+
+        return graph
+
+    def to_file(self, file_path: str):
+        """
+        Save the graph to a file.
+        """
+        with open(file_path, 'w', encoding='utf-8') as f:
+            f.write(self.model_dump_json(indent=2))
+
+    def stats(self, name: Optional[str] = None):
+        """
+        Print the stats of the graph.
+        """
+        print(f"{name or 'Graph'} with:\n\t{len(self.entities)} entities\n\t{len(self.edges)} edges\n\t{len(self.relations)} relations")
